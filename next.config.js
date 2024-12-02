dea0f3def87aaf5844b615825a6f57e86e22742f
Bailey Busch <baileybusch@gmail.com>
2024-12-02 14:10:31 -0800

    update
    update

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  distDir: '.next',
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig 
